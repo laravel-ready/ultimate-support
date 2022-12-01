acd3cc73607e86183c9fea6cb9d197766c402533
Egoist <epsilonelittr@gmail.com>
2022-12-01 17:01:16 +0300

    Add IP support configs

---
+++ config/ultimate-support.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'ip' => [
+        /**
+         * Get client public IP address if it is localhost
+         *
+         * Default: true
+         */
+        'get_local_public_ip' => true,
+
+        /**
+         * Enable/Disable get IP address failure logging
+         *
+         * Default: true
+         */
+        'log_get_ip_errors' => true,
+    ],
+];
